a63cb21596106d54600a5e80514e925ee75ca192
Eszter Fodor <chytos@gmail.com>
2013-04-29 13:01:02 +0200

    versie 2

---
+++ 3/taalmodellen3v2.py
@@ -0,0 +1,14 @@
+"""
+Assignment 3
+Eszter Fodor (5873320), Sharon Gieske (6167667) & Jeroen Rooijmans
+
+To run code: python taalmodellen3v2.py 
+"""
+
+import sys
+import time
+import math
+import re
+import operator
+from operator import itemgetter
+import itertools
